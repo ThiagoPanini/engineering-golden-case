58512163e06d189f725f85505443831856e7d71f
Thiago Panini <panini.development@gmail.com>
2025-03-05 09:32:20 -0300

    feat: definindo implementação de DynamoDB

--- infra/aws/get-b3-active-tickers/dynamodb.tf
+++ infra/aws/get-b3-active-tickers/dynamodb.tf
@@ -1,16 +1,27 @@
 /* --------------------------------------------------------
-ARQUIVO: dynamodb.tf @ get-active-tickers module
+ARQUIVO: dynamodb.tf @ get-b3-active-tickers module
 
 Criação de um DynamoDB previamente configurado como um
 recurso de armazenamento de itens obtidos através de
 interface para coleta de informações de ativos financeiros
 listados na B3.
 -------------------------------------------------------- */
-module "aws_dynamodb_table" {
-  source = "git::https://github.com/ThiagoPanini/tf-modules-showcase.git?ref=aws/dynamodb-table/v0.1.0"
 
-  name       = var.dynamodb_tickers_info_table_name
-  hash_key   = var.dynamodb_tickers_info_table_hash_key
-  range_key  = var.dynamodb_tickers_info_table_range_key
-  attributes = var.dynamodb_tickers_info_table_attributes
+module "aws_dynamodb_table" {
+  source = "git::https://github.com/ThiagoPanini/tf-modules-showcase.git?ref=aws/dynamodb-table/v0.1.1"
+
+  name      = "tbl_egc_b3_active_tickers"
+  hash_key  = "code"
+  range_key = "dt_extracted"
+
+  attributes = [
+    {
+      "name" : "code",
+      "type" : "S"
+    },
+    {
+      "name" : "dt_extracted",
+      "type" : "S"
+    }
+  ]
 }
